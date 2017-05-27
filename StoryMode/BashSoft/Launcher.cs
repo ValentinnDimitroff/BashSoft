b08a5aa928df5617de24abdba0781781ccc3077a
ValentinDimitrov <v.dimitroff@yahoo.com>
2017-05-28 00:14:57 +0300

    Stack And Queues, Sets and Dictionaries

---
+++ StoryMode/BashSoft/Launcher.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BashSoft
+{
+    public class Launcher
+    {
+        static void Main()
+        {
+            //IOManager.TraverseDirectory(@"C:\Local Disc D\Repos\BashSoft\StoryMode");
+            //IOManager.ChangeCurrentDirectoryAbsolute(@"C:\Windows");
+            //IOManager.TraverseDirectory(@"C:\Windows");
+            StudentsRepository.InitilizeData();
+            //StudentsRepository.GetAllStudentsFromCourse("Unity");
+            StudentsRepository.GetStudentScoresFromCourse("Unity", "Ivan");
+        }
+    }
+}
---
+++ StoryMode/BashSoft/StudentsRepository.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BashSoft
+{
+    public static class StudentsRepository
+    {
+        public static bool isDataInilized = false;
+        private static Dictionary<string, Dictionary<string, List<int>>> studentsByCourse;
+
+        public static void InitilizeData()
+        {
+            if (!isDataInilized)
+            {
+                OutputWriter.WriteMessageOnNewLine("Reading data...");
+                studentsByCourse = new Dictionary<string, Dictionary<string, List<int>>>();
+                ReadData();
+            }
+            else
+            {
+                OutputWriter.WriteMessageOnNewLine(ExceptionMessages.DataAlreadyInitialisedException);
+            }
+        }
+
+        private static void ReadData()
+        {
+            string input = Console.ReadLine();
+
+            while (!String.IsNullOrEmpty(input))
+            {
+                string[] tokens = input.Split(' ');
+                string course = tokens[0];
+                string student = tokens[1];
+                int mark = Int32.Parse(tokens[2]);
+
+                if (!studentsByCourse.ContainsKey(course))
+                {
+                    studentsByCourse.Add(course, new Dictionary<string, List<int>>());
+                }
+
+                if (!studentsByCourse[course].ContainsKey(student))
+                {
+                    studentsByCourse[course].Add(student, new List<int>());
+                }
+
+                studentsByCourse[course][student].Add(mark);
+                input = Console.ReadLine();
+            }
+
+            isDataInilized = true;
+            OutputWriter.WriteMessageOnNewLine("Data read!");
+        }
+
+        private static bool IsQueryForCoursePossible(string courseName)
+        {
+            if (isDataInilized)
+            {
+                if (studentsByCourse.ContainsKey(courseName))
+                {
+                    return true;
+                }
+                else
+                {
+                    OutputWriter.DisplayException(ExceptionMessages.InexistingCourseInDataBase);
+                }
+            }
+            else
+            {
+                OutputWriter.DisplayException(ExceptionMessages.DataNotInitializedExceptionMessage);
+            }
+
+            return false;
+        }
+        private static bool IsQueryForStudentPossible(string courseName, string studentUserName)
+        {
+            if (IsQueryForCoursePossible(courseName) && studentsByCourse[courseName].ContainsKey(studentUserName))
+            {
+                return true;
+            }
+            else
+            {
+                OutputWriter.DisplayException(ExceptionMessages.InexistingStudentInDataBase);
+            }
+
+            return false;
+        }
+
+        public static void GetStudentScoresFromCourse(string courseName, string username)
+        {
+            OutputWriter.PrintStudent(new KeyValuePair<string, List<int>>(username, studentsByCourse[courseName][username]));
+        }
+
+        public static void GetAllStudentsFromCourse(string courseName)
+        {
+            if (IsQueryForCoursePossible(courseName))
+            {
+                OutputWriter.WriteMessageOnNewLine($"{courseName}");
+                foreach (var studetMarksEntry in studentsByCourse[courseName])
+                {
+                    OutputWriter.PrintStudent(studetMarksEntry);
+                }
+            }
+        }
+    }
+}
